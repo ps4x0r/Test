650c235fa13088ee9e4c77cb52441d841e95407a
parmeshwar gound <gound.parmesh@gmail.com>
2016-11-17 15:58:12 +0530

    First commit

---
+++ reg.js
@@ -0,0 +1,33 @@
+﻿
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick() {
+   
+   var email = document.getElementById("email");
+    var password = document.getElementById("password")
+    email = email.value;
+    password = password.value;
+
+    if (email.value == '') {
+        alert('Provide email');
+        return false;
+    }
+
+    if (password.value == '') {
+        alert('Provide password');
+        return false;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("error code: " + errorCode);
+        console.log("error message: " + errorMessage);
+        // ...
+    });
+
+   
+    
+}
+
